8611f8d53d900c98fab93e870595c81339c0b6a8
unknown <zareennawar1@gmail.com>
2025-05-06 00:38:08 +0600

    hello

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,11 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation ("com.google.android.material:material:1.11.0")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
